4e4516bf7e3983a7eafa448e90404776df7d2b90
Elie Labeca <elie.labeca@googlemail.com>
2016-04-18 08:59:38 +0100

    [Elie] - Added offers calculation and added defaults of nil products and nil offers

--- src/main/scala/Basket.scala
+++ src/main/scala/Basket.scala
@@ -1,10 +1,28 @@
+import java.text.DecimalFormat
+
 class Basket {
-  def checkout(products: Seq[String]): Double = {
-    val total = products.foldLeft(0.0) ((total, product) => product match {
-      case "Apple"  => total + 0.60
-      case "Orange" => total + 0.25
+  val apple = "apple"
+  val applePrice = 0.60
+  val orange = "orange"
+  val orangePrice = 0.25
+
+  def checkout(products: Seq[String] = List(), discounts: Seq[Double] = List()): Double = {
+    val total = products.foldLeft(0.0) ((total, product) => product.toLowerCase match {
+      case `apple`  => total + applePrice
+      case `orange` => total + orangePrice
       case _      => total
     })
-    total
+
+    val discountTotal = (0.0 /: discounts) {_+_}
+
+    // Formatting to 2 decimal places
+    "%.2f".format(total - discountTotal).toDouble
+  }
+
+  def offer(products: Seq[String], productToDiscount: String, price: Double, howMany: Int, forThePriceOf: Int): Double = {
+    val qty = products.count(_.toLowerCase == productToDiscount)
+    val discountQty = qty / howMany
+    val multi = howMany - forThePriceOf
+    discountQty * multi * price
   }
 }
--- src/test/scala/BasketSpec.scala
+++ src/test/scala/BasketSpec.scala
@@ -7,7 +7,7 @@ class BasketSpec extends FunSpec {
     describe("when empty") {
       it("should have return a total of £0.00") {
         val basket = new Basket()
-        val total = basket.checkout(List())
+        val total = basket.checkout()
         assert(total == 0.00)
       }
     }
@@ -28,5 +28,70 @@ class BasketSpec extends FunSpec {
       }
     }
 
+    describe("when with offers of buy 1 apple and get 1 free and buy 3 oranges for the price of 2") {
+
+      describe("when there's 2 apples") {
+        it("should have a discount calculation of £0.60") {
+          val basket = new Basket()
+          val products = List("Apple", "Apple")
+          val discount = basket.offer(products, basket.apple, basket.applePrice, 2, 1)
+          assert(discount == 0.60)
+        }
+      }
+
+      describe("when there's 3 oranges") {
+        it("should have a discount calculation of £0.25") {
+          val basket = new Basket()
+          val products = List("Orange", "Orange", "Orange")
+          val discount = basket.offer(products, basket.orange, basket.orangePrice, 3, 2)
+          assert(discount == 0.25)
+        }
+      }
+
+      describe("when basket has 2 apples") {
+        it("should amount to a total of £0.60") {
+          val basket = new Basket()
+          val products = List("Apple", "Apple")
+          val discounts = List(basket.offer(products, basket.apple, basket.applePrice, 2, 1),
+            basket.offer(products, basket.orange, basket.orangePrice, 3, 2))
+          val total = basket.checkout(products, discounts)
+          assert(total == 0.60)
+        }
+      }
+
+      describe("when basket has 3 oranges") {
+        it("should amount to a total of £0.50") {
+          val basket = new Basket()
+          val products = List("Orange", "Orange", "Orange")
+          val discounts = List(basket.offer(products, basket.apple, basket.applePrice, 2, 1),
+            basket.offer(products, basket.orange, basket.orangePrice, 3, 2))
+          val total = basket.checkout(products, discounts)
+          assert(total == 0.50)
+        }
+      }
+
+      describe("when basket has 3 apples and 1 orange") {
+        it("should amount to a total of £1.45") {
+          val basket = new Basket()
+          val products = List("Apple", "Apple", "Orange", "Apple")
+          val discounts = List(basket.offer(products, basket.apple, basket.applePrice, 2, 1),
+            basket.offer(products, basket.orange, basket.orangePrice, 3, 2))
+          val total = basket.checkout(products, discounts)
+          assert(total == 1.45)
+        }
+      }
+
+      describe("when basket has 4 apples and 4 oranges") {
+        it("should amount to a total of £1.95") {
+          val basket = new Basket()
+          val products = List("Apple", "Orange", "Apple", "Orange", "Apple", "Apple", "Orange", "Orange")
+          val discounts = List(basket.offer(products, basket.apple, basket.applePrice, 2, 1),
+            basket.offer(products, basket.orange, basket.orangePrice, 3, 2))
+          val total = basket.checkout(products, discounts)
+          assert(total == 1.95)
+        }
+      }
+
+    }
   }
 }
